90f6a5c648a028fc30a27025ef5bd705c7832a56
MariamaMusa <musamariama037@gmail.com>
2026-07-22 22:34:01 +0000

    M's response

--- trials.py
+++ trials.py
@@ -2,3 +2,8 @@ def message():
     print("I am very glad I met you", "And do well to look through this", sep='?')
 
 message()
+
+def reply(name):
+    print(f"I'm not glad that I met you, {name}")
+
+reply("Lawrence")
